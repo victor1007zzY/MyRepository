9927097c13660a9295f03ed0e421729ab6a00db2
victor <1353876205@qq.com>
2015-04-09 15:05:21 +0800

    Initial commit11

---
+++ app/src/main/java/com/victor/test/MainActivity.java
@@ -0,0 +1,24 @@
+package com.victor.test;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = ((ImageView) findViewById(R.id.imageview));
+
+    }
+
+
+
+}
